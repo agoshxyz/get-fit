5699fb491619cebfe944acf2dcb95a17d792e17f
agosh-gif <agageldidurdyyev@gmail.com>
2021-11-04 15:38:54 +0200

    Added password encryption

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,26 +1,29 @@
 const User = require("../models/User");
 
+const bcrypt = require("bcrypt");
+
 module.exports = {
   async store(req, res) {
     try {
-      console.log(req.body);
       const { firstName, lastName, password, email } = req.body;
 
-      const existentUser = await Usser.findOne({ email });
+      const existentUser = await User.findOne({ email });
 
-      if (existentUser) {
+      if (!existentUser) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const user = await User.create({
-          firstName: firstName,
-          lastName: lastName,
-          password: password,
-          email: email,
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
         });
         return res.json(user);
       }
 
       return res.status(400).json({
         message:
-          "Email or user has been registered. Do you want to login instead? ",
+          "Email or user has been registered. Do you want to login instead?",
       });
     } catch (error) {
       throw Error(`Error while registering a new user ${error}`);
